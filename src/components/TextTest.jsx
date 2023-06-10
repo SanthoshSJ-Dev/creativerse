837e377ca338b47cfc7901fb73d26b63ed822882
unknown <santhoshsekar2903@gmail.com>
2023-06-10 03:35:02 -0700

    fastapi translate UI done

--- src/components/TextTest.jsx
+++ src/components/TextTest.jsx
@@ -1,29 +1,92 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "../styles/TextTest.css";
 
 const TextTest = () => {
   const [videoUrl, setVideoUrl] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
 
-  const convertVideoToAudio = async () => {
+  const linkToTranslate = async () => {
     try {
       const response = await axios.post("http://127.0.0.1:8000/download", {
-        "video_url": videoUrl,
+        video_url: videoUrl,
+        language: selectedLanguage,
       });
-      console.log(response);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const selectLanguage = (language) => {
+    setSelectedLanguage(language);
+    setShowDropdown(false);
+  };
+
+  const getButtonName = () => {
+    if (selectedLanguage === "ta") {
+      return "Tamil";
+    } else if (selectedLanguage === "te") {
+      return "Telugu";
+    } else selectedLanguage === "zh-cn";
+    return "Chinese";
+  };
+
   return (
     <>
-      <h1>Download</h1>
-      <input
-        type="text"
-        value={videoUrl}
-        onChange={(e) => setVideoUrl(e.target.value)}
-      />
-      <button onClick={convertVideoToAudio}>Convert</button>
+      <div className="translate">
+        <h1 className="title">Translate</h1>
+        <div className="translate_container">
+          <div className="input">
+            <label htmlFor="url">Enter URL</label>
+            <input
+              id="url"
+              className="translate_url_input"
+              type="url"
+              value={videoUrl}
+              onChange={(e) => setVideoUrl(e.target.value)}
+            />
+          </div>
+
+          <div className="dropdown_container">
+            <label htmlFor="">Select Language</label>
+            <div className="dropdown">
+              <button className="dropdown-toggle" onClick={toggleDropdown}>
+                {getButtonName()}
+              </button>
+              {showDropdown && (
+                <div className="dropdown-menu">
+                  <button
+                    className="dropdown-item"
+                    onClick={() => selectLanguage("ta")}
+                  >
+                    Tamil
+                  </button>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => selectLanguage("te")}
+                  >
+                    Telugu
+                  </button>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => selectLanguage("zh-cn")}
+                  >
+                    Chinese
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+          <button className="translate_btn" onClick={linkToTranslate}>
+            Translate
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import {auth} from '../config/firebase';
-import LoginBtn from "../components/Navbar/LoginBtn";
 import TextTest from "../components/TextTest";
 
 const Products = () => {
   return (
-    <div>
-      <h1 className="padding">Products</h1>
-      {/* <LoginBtn auth={auth} /> */}
-
-      <TextTest/>
+    <div className="padding">
+      <TextTest />
     </div>
   );
 };
